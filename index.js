8581e60a70cf2291860cf1a9763bad34a6543adb
EvgeniyRyabchuk <jeka.rubchuk@gmail.ocm>
2024-01-11 21:07:25 +0200

    dfg

---
+++ index.js
@@ -0,0 +1,120 @@
+
+
+"./images/nature_tiny_ver_1.png"
+"./images/nature_small_ver_1.png"
+"./images/travel_small_hor_1.png"
+"./images/text_ver_1.png"
+"./images/animal_sq_1.png"
+"./images/food_ver_1.png"
+"./images/people_nature_1_ver.png"
+"./images/nature_hor_2.png"
+
+const imageCount = 8;
+
+const pathGenerator = (count, text, ext, basename) => {
+    const paths = []; 
+    for(let i = count; i > 0; i--) {
+        paths.push(`${basename}/res_${text}_${i}.${ext}`)
+    }
+    return paths; 
+}
+
+const imageTypes = [
+    {
+        name: 'jpeg2000',
+        text: "jpeg",
+        ext: 'jpg'
+    },
+    {
+        name: 'libjpegturbo',
+        text: "jpeg_turbo",
+        ext: 'jpg'
+    },
+    {
+        name: 'mozjpeg',
+        text: "jpegMOZ", 
+        ext: 'jpg'
+    },
+    {
+        name: 'guetzli',
+        text: "jpegGUETZLI",
+        ext: 'jpg'
+    },
+    {
+        name: 'webp',
+        text: "webp",
+        ext: 'webp'
+    },
+    {
+        name: 'avif',
+        text: "avif",
+        ext: 'avif',
+    },
+    {
+        name: 'flif',
+        text: "flif",
+        ext: 'flif'
+    },
+    {
+        name: 'jpegxl',
+        text: "jxl",
+        ext: 'jxl'
+    },
+    {
+        name: 'bpg',
+        text: "bpg",
+        ext: 'bpg'
+    }
+];
+
+
+const imageType = imageTypes.find(it => it.name === selectedType); 
+
+const imagePahtList = pathGenerator(imageCount, imageType.text, imageType.ext, "../images");
+
+console.log(imagePahtList);
+
+const imageContainer = document.querySelector('.image_container');
+
+// if(imageType.ext === "bpg") || imageType.ext === "flif" || imageType.ext === "jxl") {
+//     for(let path of imagePahtList) {
+//         imageContainer.innerHTML += 
+//         `<img src=\"${path}\">`
+//     }
+// }
+
+for(let path of imagePahtList) {
+    if(imageType.ext === "bpg" || imageType.ext === "flif" || imageType.ext === "jxl") {
+        imageContainer.innerHTML += `<img src=\"${path}\">`; 
+    } else {
+        const pngPath = path.replace(`.${imageType.ext}`, "")+".png"
+        console.log(pngPath); 
+        const htmlText =  
+        "<picture>"+
+            `<source srcset=\"${path}\" type=\"image/avif\">`+
+            `<img src=\"${pngPath}\">`+ 
+        "</picture>"
+        imageContainer.innerHTML += htmlText; 
+        console.log("123");
+    }
+      
+    
+    // for(let path of imagePahtList) {
+    //     const imageTag = document.createElement("img");
+    //     imageTag.setAttribute("src", path); 
+    //     imageContainer.appendChild(imageTag); 
+    // } 
+}
+
+ 
+
+
+console.log(imageContainer);
+
+
+
+console.log("Time until DOMready: ", Date.now()-timerStart);
+
+window.onload = function () {
+    console.log("Time until everything loaded: ", Date.now()-timerStart);
+};
